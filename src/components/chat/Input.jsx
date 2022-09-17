a860af5ec7e381590a05715a0d329a635a8fcdfd
cris-gs <cristophergs2001@gmail.com>
2022-09-17 11:33:52 -0600

    ajuste de responsive en tablet y mobile

--- src/components/chat/Input.jsx
+++ src/components/chat/Input.jsx
@@ -3,12 +3,12 @@ const icons = require.context('../../assets', true);
 export const Input = () => {
   return (
     <div className="input">
-      <input type="text" placeholder="Write Something.." />
+      <input type="text" placeholder="Write Some.." />
       <div className="send">
       <img src={icons('./microphone.svg')} alt="" />
         <img src={icons('./clip.svg')} alt="" />
         <input type="file" style={{display:"none"}} id="file" />
-        <label htmlFor="file">
+        <label htmlFor="file" className="displayImg">
           <img src={icons('./photo.svg')} alt="" />
         </label>
         <button>Send</button>
